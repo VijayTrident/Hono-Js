c202a10e9cf78c6d0696675cdb4f0d3e9e1b2af3
VijayTrindet <vijay.babu@tridentsqa.com>
2026-07-16 15:17:37 +0530

    Added basepath

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { Hono } from 'hono'
 import { connectDB } from './config/db.js'
 import bookRoutes from './routes/books.js'
 
-const app = new Hono()
+const app = new Hono().basePath('/api');
 
 app.route('/books', bookRoutes)
 
-await connectDB()
+await connectDB();
 
 serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
-  console.log(`Bookstore API running on http://localhost:${info.port}`)
-})
+  console.log(`Bookstore API running on http://localhost:${info.port}`);
+});
